d325bcf404b8967cb29b3e64e7b107eb5ab3ab9e
sneha <snehabhagat375@gmail.com>
2026-08-25 00:19:27 +0530

    Add solution for largest element

---
+++ Largest-element/Largest-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestElement(int[] nums) {
+        int largest=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>largest){
+                largest=nums[i];
+            }
+        }
+        return largest;
+    
+    }
+}
