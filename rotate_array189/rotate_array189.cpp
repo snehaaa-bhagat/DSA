daa28d6e999b096c928fb7ba0a0120e7467a4e14
sneha <snehabhagat375@gmail.com>
2026-09-04 02:54:57 +0530

    Add solution for rotate array by k places in right

---
+++ rotate_array189/rotate_array189.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // void reverse_arr(vector<int>& nums, int start, int end){
+    //     while(start<=end){
+    //         int temp=nums[start];
+    //         nums[start]=nums[end];
+    //         nums[end]=temp;
+    //         start++;
+    //         end--;
+
+    //     }
+    // }
+    
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        // reverse_arr(nums,0,n-1);
+        // reverse_arr(nums,0,k-1);
+        // reverse_arr(nums,k,n-1);
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
+    }
+    
+    
+
+};
