03f6f90b7d33412d3d493aa5ed69a3ce694058d9
sneha <snehabhagat375@gmail.com>
2026-09-19 01:35:32 +0530

    Add solution to find number that appear once in an array

---
+++ find-single-appeared-number/find-single-appeared-number.cpp
@@ -0,0 +1,21 @@
+class Solution{    
+public:    
+    int singleNumber(vector<int>& nums){
+        //your code goes here
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int cnt=0;
+
+            for(int j=0;j<n;j++){
+                if(nums[j]==num){
+                    cnt++;
+                }
+            }
+            if(cnt==1){
+                return num;
+            }
+        }
+    return -1;
+    }
+};
