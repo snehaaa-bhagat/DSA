a8c622bccca480518856daaeed279b0cf4443595
sneha <tanubhagat382@gmail.com>
2026-09-23 02:49:23 +0530

    Add solution to find third max number leet414

---
+++ third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long first=LLONG_MIN, second=LLONG_MIN, third=LLONG_MIN;
+        for(int num: nums){
+            if(num==first || num==second || num==third){
+                continue;
+            }
+
+            if(num>first){
+                third=second;
+                second=first;
+                first=num;
+            }
+            else if(num>second){
+                third=second;
+                second=num;
+
+            }
+            else if(num>third){
+                third=num;
+            }
+        }
+        if(third==LLONG_MIN){
+            return first;
+        }
+        else{
+            return third;
+        }
+        
+
+
+        
+    }
+};
